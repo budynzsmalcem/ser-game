77fe1739e2aabf664b4ab67c0f3f2b32c6cffe63
budynzsmalcem <74183055+budynzsmalcem@users.noreply.github.com>
2020-11-11 18:04:22 +0100

    Update ser.py

--- ser/ser.py
+++ ser/ser.py
@@ -10,10 +10,31 @@ pygame.display.set_caption(" Ser")
 portret = pygame.image.load('ser.png')
 pygame.display.set_icon(portret)
 
+blockimg = pygame.image.load('basicblock.png')
+xblock = 275
+yblock = 400
+
+nol = 2
+
+def block(x,y,nol):
+    k = 0
+    while nol > k:
+        if k == 0:
+            screen.blit(blockimg, (x, ((y-(nol*40)))))
+        else:
+            for z in range(nol+2):
+                screen.blit(blockimg, ((x-60)+60*z, y))
+        k+=1
+
 running = True
 while running:
+    
+    screen.fill(( 255,255,255 )
+                
     for events in pygame.event.get():
         if events.type == pygame.QUIT:
             running = False
-    screen.fill(( 255,255,255 ))
+    
+    block(xblock, yblock, nol)
+    
     pygame.display.update()
